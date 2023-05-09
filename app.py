212ef81661be2638b54816bf26aa498ba70cc91e
codesbydre <gabaldonandrea@gmail.com>
2023-05-09 12:46:11 -0700

    commit step 8 to survey app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_debugtoolbar import DebugToolbarExtension
+from flask import session
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
@@ -18,20 +19,23 @@ def start():
 
 @app.route("/questions/<int:question_id>")
 def show_question(question_id):
-    if question_id != len(responses) and question_id != 0:
+    if question_id != len(session["responses"]) and question_id != 0:
         flash("Invalid question ID.")
-        return redirect(url_for("show_question", question_id=len(responses)))
-    if question_id >= len(satisfaction_survey.questions):
-        return redirect(url_for("thank_you"))
+        return redirect(url_for("show_question", question_id=len(session["responses"])))
+
     question = satisfaction_survey.questions[question_id]
-    return render_template("question.html", question=question)
+    return render_template("question.html", question=question, question_id=question_id)
 
 
 @app.route("/answer", methods=["POST"])
 def handle_answer():
     answer = request.form["answer"]
-    responses.append(answer)
-    next_question_id = len(responses)
+
+    session["responses"].append(answer)
+    session.modified = True
+
+    next_question_id = len(session["responses"])
+
     if next_question_id >= len(satisfaction_survey.questions):
         return redirect(url_for("thank_you"))
     else:
@@ -41,3 +45,9 @@ def handle_answer():
 @app.route("/thank-you")
 def thank_you():
     return render_template("thank_you.html")
+
+
+@app.route("/start-survey", methods=["POST"])
+def start_survey():
+    session["responses"] = []
+    return redirect(url_for("show_question", question_id=0))
